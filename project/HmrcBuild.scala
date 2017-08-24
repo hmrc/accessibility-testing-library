52391c1656f5f805865b8d06bd79475facc0c14f
Chris Wright <chris.wright@digital.hmrc.gov.uk>
2017-08-24 09:05:23 +0100

    Fixing test dependencies. Adding missing license block

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import sbt.Keys._
 import sbt._
@@ -29,6 +44,8 @@ private object AppDependencies {
   lazy val test = Seq(
     "org.seleniumhq.selenium" % "selenium-java"%"2.53.0",
     "org.scalatest" % "scalatest_2.11" % "2.2.1",
+    "org.mockito" % "mockito-all" % "1.10.19" % "test",
+    "org.pegdown" % "pegdown" % "1.6.0",
     "info.cukes" % "cucumber-scala_2.11" % "1.2.2",
     "info.cukes" % "cucumber-java" % "1.2.2",
     "junit" % "junit" % "4.11" % "test",
