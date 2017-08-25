88e7a708b767060566f7ddacc638d5d02745cd90
Chris Wright <chris.wright@digital.hmrc.gov.uk>
2017-08-25 09:28:22 +0100

    Improving unit test format for Reports

--- src/test/scala/uk/gov/hmrc/accessibility/AccessibilityReportSpec.scala
+++ src/test/scala/uk/gov/hmrc/accessibility/AccessibilityReportSpec.scala
@@ -61,36 +61,22 @@ class AccessibilityReportSpec extends WordSpec with Matchers with MockitoSugar {
     doc.getElementsContainingOwnText("WARNING").size() shouldBe warnings
   }
 
+  val tables = Table(
+    ("errors", "warnings", "message"),
+    (0, 0, "contain a header and no rows for empty result sequence"),
+    (1, 0, "contain a header and one row for result sequence with 1 error"),
+    (0, 1, "contain a header and one row for result sequence with 1 warning"),
+    (1, 1, "contain a header and two rows for result sequence with 1 error and 1 warning"),
+    (2, 2, "contain a header and four rows for result sequence with 2 errors and 2 warnings")
+  )
+
   "makeTable" should {
-    "contain a header and no rows for empty result sequence" in {
-      val output: Document = Jsoup.parse(makeTable(Seq()))
-      checkTableHead(output)
-      checkTableRowCount(output, 0, 0)
-    }
-
-    "contain a header and one row for result sequence with 1 error" in {
-      println(makeSeq(1, 1))
-      val output: Document = Jsoup.parse(makeTable(makeSeq(1, 0)))
-      checkTableHead(output)
-      checkTableRowCount(output, 1, 0)
-    }
-
-    "contain a header and one row for result sequence with 1 warning" in {
-      val output: Document = Jsoup.parse(makeTable(makeSeq(0, 1)))
-      checkTableHead(output)
-      checkTableRowCount(output, 0, 1)
-    }
-
-    "contain a header and two rows for result sequence with 1 error and 1 warning" in {
-      val output: Document = Jsoup.parse(makeTable(makeSeq(1, 1)))
-      checkTableHead(output)
-      checkTableRowCount(output, 1, 1)
-    }
-
-    "contain a header and four rows for result sequence with 2 errors and 2 warnings" in {
-      val output: Document = Jsoup.parse(makeTable(makeSeq(2, 2)))
-      checkTableHead(output)
-      checkTableRowCount(output, 2, 2)
+    forAll(tables) {(e, w, msg) =>
+      msg in {
+        val output: Document = Jsoup.parse(makeTable(makeSeq(e, w)))
+        checkTableHead(output)
+        checkTableRowCount(output, e, w)
+      }
     }
   }
 }
